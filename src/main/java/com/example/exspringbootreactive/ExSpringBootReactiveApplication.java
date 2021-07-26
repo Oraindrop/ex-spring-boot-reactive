bf57e6376fc5410bb5d0182527b3ce1f82531bfc
Choising <chltmdals115@gmail.com>
2021-07-27 03:45:07 +0900

    first commit

---
+++ src/main/java/com/example/exspringbootreactive/ExSpringBootReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.exspringbootreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExSpringBootReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExSpringBootReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exspringbootreactive/ExSpringBootReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exspringbootreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExSpringBootReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
